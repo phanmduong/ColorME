9c336e9db2bf52ecfa30c0a751ffc0495cffbb8d
hoangtubatu123 <kiemkhachdatinh2@gmail.com>
2017-12-21 16:29:42 +0700

    control noti

--- src/App.js
+++ src/App.js
@@ -10,28 +10,63 @@ import OneSignal from "react-native-onesignal";
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 class App extends React.Component {
-
+   constructor(){
+       super();
+       this.onOpened = this.onOpened.bind(this);
+       this.onReceived = this.onReceived.bind(this);
+   }
+    // componentWillMount() {
+    //     OneSignal.sendTag("device_type", "mobile");
+    // }
+    //
+    // componentWillUnmount() {
+    //     OneSignal.removeEventListener('opened', this.onOpened);
+    //     OneSignal.deleteTag("device_type");
+    // }
+    //
+    // onOpened(openResult) {
+    //     console.log(openResult.notification.payload.launchURL);
+    //     this.navigate(openResult.notification.payload.launchURL);
+    // }
+    // navigate (url) { // E
+    //     const { navigate } = this.props.navigation;
+    //     const route = url.replace(/.*?:\/\//g, '');
+    //     const routeName = route.split('/')[1];
+    //     if (routeName === 'project') {
+    //         navigate('Main');
+    //     };
+    // }
     componentWillMount() {
+        OneSignal.addEventListener('received', this.onReceived);
         OneSignal.addEventListener('opened', this.onOpened);
-        OneSignal.sendTag("device_type", "mobile");
+        OneSignal.addEventListener('registered', this.onRegistered);
+        OneSignal.addEventListener('ids', this.onIds);
     }
 
     componentWillUnmount() {
+        OneSignal.removeEventListener('received', this.onReceived);
         OneSignal.removeEventListener('opened', this.onOpened);
-        OneSignal.deleteTag("device_type");
+        OneSignal.removeEventListener('registered', this.onRegistered);
+        OneSignal.removeEventListener('ids', this.onIds);
+    }
+
+    onReceived(notification) {
+        console.log("Notification received: ", notification);
     }
 
     onOpened(openResult) {
-        console.log(openResult);
-        this.setState({URL : openResult.notification.payload.launchURL});
+        console.log('Message: ', openResult.notification.payload.body);
+        console.log('Data: ', openResult.notification.payload.additionalData);
+        console.log('isActive: ', openResult.notification.isAppInFocus);
+        console.log('openResult: ', openResult);
     }
-    navigate (url) { // E
-        const { navigate } = this.props.navigation;
-        const route = url.replace(/.*?:\/\//g, '');
-        const routeName = route.split('/')[1];
-        if (routeName === 'project') {
-            navigate('Login');
-        };
+
+    onRegistered(notifData) {
+        console.log("Device had been registered for push notifications!", notifData);
+    }
+
+    onIds(device) {
+        console.log('Device info: ', device);
     }
 
     render() {
@@ -43,13 +78,9 @@ class App extends React.Component {
     }
     componentDidMount() {
         OneSignal.inFocusDisplaying(2);
-        if (Platform.OS === 'android') {
-            Linking.getInitialURL().then((url) => {
-                this.navigate(url);
-            });
-        } else {
-            Linking.addEventListener('url', this.navigate(this.state.URL));
-        }
+        OneSignal.configure({
+            onNotificationOpened : this.onOpened
+        })
     }
 }
 
