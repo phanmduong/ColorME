8c8eba8e90aa294a9fef2bd1b5ee6daf83cc0656
Phan Minh Dương <phanminhduong@hotmail.com>
2018-11-28 11:46:31 +0700

    update code

--- src/modules/login/loginApi.js
+++ src/modules/login/loginApi.js
@@ -1,13 +1,13 @@
-
 import axios from 'axios';
 import {APIS} from "../../constants/env";
 import DeviceInfo from 'react-native-device-info';
+
 export function loginApi(login) {
-   let url = APIS.COLOR_ME + "/login"
+    let url = APIS.COLOR_ME + "/login"
     return axios.post(url, {
-        email: login.email,
+        email: login.email.charAt(0).toLowerCase() + login.email.substr(1),
         password: login.password,
-    }) 
+    })
 };
 // export function loadCheckDevice(device, token) {
 //     let url = "http://manageapi.colorme.vn" + "/checkincheckout/check-device?token=" + token;
